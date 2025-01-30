bd5ddf9c72b44dac049ed7e47a69ffdedc859838
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-01-30 21:50:57 +0100

    Create first Record Class and check if we can create an Instance of this recrod in Logger

---
+++ src/main/java/dev/spring_boot_web_API/runnerz/run/Location.java
@@ -0,0 +1,5 @@
+package dev.spring_boot_web_API.runnerz.run;
+
+public enum Location {
+    INDOOR, OUTDOOR
+}
---
+++ src/main/java/dev/spring_boot_web_API/runnerz/run/Run.java
@@ -0,0 +1,12 @@
+package dev.spring_boot_web_API.runnerz.run;
+
+import java.time.LocalDateTime;
+
+public record Run(
+        Integer id,
+        String title,
+        LocalDateTime startedOn,
+        LocalDateTime completedOn,
+        Integer miles,
+        Location location
+){}
