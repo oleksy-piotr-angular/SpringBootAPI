e260f78b3891a05e3623299feb51a9ab3124b048
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-01-30 10:53:39 +0100

    Init

---
+++ src/main/java/dev/spring_boot_web_API/runnerz/Application.java
@@ -0,0 +1,13 @@
+package dev.spring_boot_web_API.runnerz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev/spring_boot_web_API/runnerz/RunnerzApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.spring_boot_web_API.runnerz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunnerzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
